60ff2161841ab252ab040e2f33861cfd058131f7
Tom Mount <tmountjr@gmail.com>
2015-11-30 00:32:25 -0500

    mostly working; correctly pulls, parses, and compares inventories; sends email if new items were found.

--- index.js
+++ index.js
@@ -2,13 +2,83 @@
 
 var fs = require('fs'),
 	nconf = require('nconf'),
-	request = require('request'),
-	cheerio = require('cheerio');
+	http = require('http'),
+	cheerio = require('cheerio'),
+	requestObject,
+	oldInventory,	// loaded from disk or memory
+	newInventory;	// parsed from raw html request response
 
-var oldInventory = importInventory('./oldInventory.json');
-var newInventory = importInventory('./newInventory.json');
+nconf.file('config.json');
 
-console.log(compareInventories(oldInventory, newInventory));
+// dropping this here so things can be changed later on if necessary
+requestObject = {
+	registryId: "542647053",
+	startIdx: "0",
+	isGiftGiver: true,
+	blkSize: "1000",
+	isAvailForWebPurchaseFlag: false,
+	userToken: "UT1021",
+	sortSeq: "1",
+	view: "3",
+	eventTypeCode: "BRD",
+	eventType: "Wedding",
+	pwsurl: '',
+	totalToCopy: 85
+};
+
+var options = {
+	'host': 'www.bedbathandbeyond.com',
+	'path': '/store/giftregistry/frags/registry_items_guest.jsp?' + toQueryString(requestObject),
+	'headers': {
+		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36'
+	}
+};
+
+var callback = function(response) {
+	var str = '';
+
+	response.on('data', function(chunk) {
+		str += chunk;
+	});
+
+	// all the good stuff happens here
+	response.on('end', function() {
+		oldInventory = importInventory('./inventory.json');
+		newInventory = parseResponse(str);
+
+		compared = compareInventories(oldInventory, newInventory);
+		// console.log(compared);
+
+		if (Object.keys(compared).length == 0) {
+			// nothing new found
+		} else {
+			var $ = cheerio.load(str),
+				emailBody = "The following new items have been found:\n";
+
+			for (var category in compared) {
+				emailBody += "\n\tCategory: " + category + "\n";
+				for (var i = 0; i < compared[category].length; i++) {
+					var sku = compared[category][i];
+
+					// search the raw response for a matching sku
+					var title = $("span.blueName a[data-skuid=" + sku + "]").text().replace(/\s\(\d+\)$/, '');
+					if (title == '') title = '(No title found. Check the website for details.)';
+					emailBody += "\t" + title + "\n";
+				}
+			}
+
+			// console.log(emailBody);
+			sendEmail(
+				nconf.get("smtp:from"),
+				nconf.get("smtp:to"),
+				'New Wedding Registry Items!',
+				emailBody
+			);
+		}
+	});
+}
+
+http.request(options, callback).end();
 
 
 ///////////////////////
@@ -21,13 +91,26 @@ function toTitleCase(str) {
     });
 }
 
+function toQueryString(obj) {
+    var parts = [];
+    for (var i in obj) {
+        if (obj.hasOwnProperty(i)) {
+            parts.push(encodeURIComponent(i) + "=" + encodeURIComponent(obj[i]));
+        }
+    }
+    return parts.join("&");
+}
+
 function parseResponse(rawHtmlResponse) {
 	var $ = cheerio.load(rawHtmlResponse),
-		inventory = {}
+		inventory = {};
 
 	$("h2.ui-accordion-header").each(function(h2Index, h2Element) {
 
 		var thisCategoryName = $(this).find('a').text().replace(/\s\(\d+\)$/, '');
+
+		if (inventory[thisCategoryName])
+			thisCategoryName += " [ISO]"	// this means the category is in-store only, not available online
 
 		inventory[thisCategoryName] = [];
 
@@ -35,9 +118,9 @@ function parseResponse(rawHtmlResponse) {
 			var thisCategoryList = {},
 				hiddenInputs = $(this).find('li.productLastColumn');
 
-			thisCategoryList.title = $(this).find('li.productName span.blueName a').text().trim();
+			// thisCategoryList.title = $(this).find('li.productName span.blueName a').text().trim();
 			thisCategoryList.sku = $(hiddenInputs).find('input[name=skuId]').val();
-			thisCategoryList.pid = $(hiddenInputs).find('input[name=prodId]').val();
+			// thisCategoryList.pid = $(hiddenInputs).find('input[name=prodId]').val();
 
 			inventory[thisCategoryName].push(thisCategoryList.sku);
 		});
@@ -62,7 +145,7 @@ function compareInventories(prevInventory, newInventory) {
 	
 	var toReturn = {};
 	
-	for (newInventoryCategory in newInventory) {
+	for (var newInventoryCategory in newInventory) {
 
 
 		if (! prevInventory[newInventoryCategory]) {
@@ -91,4 +174,28 @@ function compareInventories(prevInventory, newInventory) {
 	}
 
 	return toReturn;
+}
+
+function sendEmail(from, to, subject, body) {
+	var nodemailer = require('nodemailer'),
+		smtpTransport = require('nodemailer-smtp-transport'),
+		transporter;
+
+	transporter = nodemailer.createTransport(smtpTransport({
+		'host': 'smtp.1and1.com',
+		'port': 587,
+		'auth': {
+			'user': nconf.get('smtp:username'),
+			'pass': nconf.get('smtp:password')
+		}
+	}));
+
+	transporter.sendMail({
+		'from': from,
+		'to': to,
+		'subject': subject,
+		'text': body
+	}, function(err, info) {
+		if (err) throw err;
+	});
 }
