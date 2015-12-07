1086b28d2694e699e8275d79ef8f3af519ce0171
Tom Mount <tmountjr@gmail.com>
2015-12-07 09:55:58 -0500

    added some tests for the inventory comparison to test cases beyond just "these are the same"

--- test/test_support.js
+++ test/test_support.js
@@ -2,73 +2,9 @@ var s = require('../lib/support.js'),
 	pwd = process.env.PWD;
 
 module.exports = {
+
 	testLoadConfig: function(test) {
 		test.ok(s.loadConfig(pwd + '/lib/config.json') instanceof Object);
-		test.done();
-	},
-
-	testInventoryImport: function(test) {
-		test.doesNotThrow(function() {
-			return s.inventory.import(pwd + '/lib/inventory.json');
-		});
-		test.ok(s.inventory.import(pwd + '/lib/inventory.json') instanceof Object);
-		test.done();
-	},
-
-	testInventorySave: function(test) {
-		var fs = require('fs'),
-			mock = require('mock-fs'),
-			toWrite = {
-				"a": [
-					"z",
-					"y"
-				],
-				"b": [
-					"x",
-					"w"
-				]
-			};
-
-		// mock the filesystem so we can write an inventory file, then read it back as a parsed
-		// JSON object and make sure it's a deep equal match to what was written. Tests the
-		// inventory.save function without relying on inventory.import.
-
-		mock({
-			'../lib': {
-				'inventory.json': 'foo'
-			}
-		});
-		test.doesNotThrow(function() {
-			s.inventory.save('../lib/inventory.json', toWrite);
-		});
-		test.deepEqual(toWrite, JSON.parse(fs.readFileSync('../lib/inventory.json')));
-		mock.restore();
-		
-		test.done();
-	},
-
-	testInventoryCompare: function(test) {
-		var expected = {},
-			oldInventory = newInventory = {
-				"cat1": {
-					"sku1": {
-						"requested": 1,
-						"purchased": 2
-					},
-					"sku2": {
-						"requested": 3,
-						"purchased": 4
-					}
-				},
-				"cat2": {
-					"sku3": {
-						"requested": 5,
-						"purchased": 6
-					}
-				}
-			};
-
-		test.deepEqual(expected, s.inventory.compare(oldInventory, newInventory));
 		test.done();
 	},
 
@@ -90,5 +26,181 @@ module.exports = {
 
 		test.equal(expected, s.toQueryString(input));
 		test.done();
+	},
+
+	inventory: {
+		setUp: function(callback) {
+			this.baseline = {
+				"cat1": {
+					"sku1": {
+						"requested": 1,
+						"purchased": 2
+					},
+					"sku2": {
+						"requested": 3,
+						"purchased": 4
+					}
+				},
+				"cat2": {
+					"sku3": {
+						"requested": 5,
+						"purchased": 6
+					}
+				}
+			};
+
+			this.newCategory = {
+				"cat1": {
+					"sku1": {
+						"requested": 1,
+						"purchased": 2
+					},
+					"sku2": {
+						"requested": 3,
+						"purchased": 4
+					}
+				},
+				"cat2": {
+					"sku3": {
+						"requested": 5,
+						"purchased": 6
+					}
+				},
+				"cat3": {
+					"sku4": {
+						"requested": 7,
+						"purchased": 8
+					}
+				}
+			};
+
+			this.newSku = {
+				"cat1": {
+					"sku1": {
+						"requested": 1,
+						"purchased": 2
+					},
+					"sku2": {
+						"requested": 3,
+						"purchased": 4
+					}
+				},
+				"cat2": {
+					"sku3": {
+						"requested": 5,
+						"purchased": 6
+					},
+					"sku4": {
+						"requested": 7,
+						"purchased": 8
+					}
+				}
+			};
+
+			this.newPurchase = {
+				"cat1": {
+					"sku1": {
+						"requested": 1,
+						"purchased": 2
+					},
+					"sku2": {
+						"requested": 3,
+						"purchased": 4
+					}
+				},
+				"cat2": {
+					"sku3": {
+						"requested": 5,
+						"purchased": 7
+					}
+				}
+			};
+
+			callback();
+		},
+
+		tearDown: function(callback) {
+			callback();
+		}
+
+		testInventoryImport: function(test) {
+			test.doesNotThrow(function() {
+				return s.inventory.import(pwd + '/lib/inventory.json');
+			});
+			test.ok(s.inventory.import(pwd + '/lib/inventory.json') instanceof Object);
+			test.done();
+		},
+
+		testInventorySave: function(test) {
+			var fs = require('fs'),
+				mock = require('mock-fs'),
+				toWrite = this.baseline;
+
+			// mock the filesystem so we can write an inventory file, then read it back as a parsed
+			// JSON object and make sure it's a deep equal match to what was written. Tests the
+			// inventory.save function without relying on inventory.import.
+
+			mock({
+				'../lib': {
+					'inventory.json': 'foo'
+				}
+			});
+			test.doesNotThrow(function() {
+				s.inventory.save('../lib/inventory.json', toWrite);
+			});
+			test.deepEqual(toWrite, JSON.parse(fs.readFileSync('../lib/inventory.json')));
+			mock.restore();
+			
+			test.done();
+		},
+
+		testInventoryCompareNoDifferences: function(test) {
+			var expected = {};
+
+			test.deepEqual(expected, s.inventory.compare(this.baseline, this.baseline));
+			test.done();
+		},
+
+		testInventoryCompareNewCategory: function(test) {
+			var expected = {
+				"cat3": {
+					"sku4": {
+						"requested": 7,
+						"purchased": 8
+					}
+				}
+			};
+
+			test.deepEqual(expected, s.inventory.compare(this.baseline, this.newCategory));
+			test.done();
+		},
+
+		testInventoryCompareNewSku: function(test) {
+			var expected = {
+				"cat2": {
+					"sku4": {
+						"requested": 7,
+						"purchased": 8
+					}
+				}
+			};
+
+			test.deepEqual(expected, s.inventory.compare(this.baseline, this.newSku));
+			test.done();
+		},
+
+		testInventoryCompareNewItemPurchased: function(test) {
+			var expected = {
+				"cat2": {
+					"sku3": {
+						"requested": 5,
+						"purchased": 7
+					}
+				}
+			};
+
+			test.deepEqual(expected, s.inventory.compare(this.baseline, this.newPurchase));
+			test.done();
+		}
 	}
 }
